6fd8fed0d427db446650bbf32076a4f73bf1ac11
Volodimir1 <vladimirn062012@gmail.com>
2020-07-07 00:52:13 +0300

    s-12-varsandmixins

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Hello, pacani!"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #FDB100;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Bauhaus, Helvetica, Arial, sans-serif;
+  // font-family: 'Modak', cursive;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #FDB100;
+  // margin-top: 30px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="header">
+    <h1 class="header__title">Site Store</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+// <style scoped lang="scss">
+.header {
+    // margin: 0;
+    &__title {
+       font-family: 'Modak', cursive;
+    //    @include text($H700, 400, $M1);
+            //   color: $M1;
+
+    //    font-size: 3em;
+    //    font-weight: normal;
+        // -webkit-font-smoothing: antialiased;
+        // -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        // color: #FDB100;
+        margin-top: 30px; 
+    }
+}
+</style>
