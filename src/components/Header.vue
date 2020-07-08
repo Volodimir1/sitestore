1720b1d4868405d1a98186f4c823ec90aa0993c2
Volodimir1 <vladimirn062012@gmail.com>
2020-07-09 01:41:24 +0300

    s-16

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,12 @@
 <template>
 <div class="header">
-    <img class="header__image" src="../images/sites2logo.svg" alt="logo">
-    <h1 class="header__title">Site Store</h1>
+    <div class="header-item header__title">
+      <img class="header__title-item header__image" src="../images/sites2logo.svg" alt="logo">
+      <h1 class="header__title-item header__title-head">Site Store</h1>
+    </div>
+    <div class="header-item header__nav">
+
+    </div>
 </div>
 </template>
 
@@ -16,16 +21,33 @@ export default {
 
 // <style scoped lang="scss">
 .header {
-    // margin: 0;
-    &__image {
-      height: 30px;
-      width: 30px;
+  background-color: $N600;
+  @include flex(space-around, center);
+  &__title {
+    @include flex(space-around, center);
+  }
+  &__title-item {
+    &:not(:last-child) {
+      margin-right: 10px;
     }
-    &__title {
-      font-family: $title-font, cursive;
-      @include text($H800, 400, $M1);
-      text-align: center;
-      // margin-top: 30px; 
+  }
+  &__image {
+    @include svg(35px, $N800)
+    &:hover {
+      transform: translateY(-5px);
+      box-shadow: 0 0 10px $T200;
+    }
+  }
+  &__title-head {
+    font-family: $title-font, cursive;
+    @include text($H800, 400, $M1);
+    text-align: center;
+    // margin-top: 30px; 
+  }
+}
+.header-item {
+  &:not(:last-child) {
+      margin-right: 40px;
     }
 }
 </style>
